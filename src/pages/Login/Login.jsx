07e41bb17328277ed2f25026d21c4ff52a9984b6
Laura Gómez Cabero <lauragcabero@gmail.com>
2020-07-11 21:18:07 +0200

    TabComponent

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,14 +1,27 @@
 import React from 'react';
 import { TabComponent } from '../../modules/TabComponent/TabComponent';
+import { SocialLoginComponent } from '../../modules/SocialLoginComponent/SocialLoginComponent';
+import { Container } from '@material-ui/core';
 
 export function Login() {
 
   const tabs = [
-    { label: 'Iniciar sesión', link: '/login', selected: true },
-    { label: 'Regístrate', link: '/register' }
-  ] 
+    { 
+      label: 'Iniciar sesión', 
+      link: '/login', 
+      selected: true 
+    },
+    { 
+      label: 'Regístrate', 
+      link: '/register' 
+    }
+  ]
 
   return (
-    <TabComponent tabs={tabs}/>
+    <Container maxWidth="lg">
+      <TabComponent tabs={tabs} />
+      <h1>Inicia sesión ahora</h1>
+      <SocialLoginComponent />
+    </Container>
   )
 }
