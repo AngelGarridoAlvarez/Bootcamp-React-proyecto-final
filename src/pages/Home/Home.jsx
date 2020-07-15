a947fbd93b1ae2b288bcd1ad4409829e2bfe8f43
@AngelGarrido <angelgarridoalvarez@gmail.com>
2020-07-15 14:55:28 +0200

    maquetación fake previa de home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,16 +1,48 @@
+import "./Home.scss"
 import React from 'react';
 import BottomNav from "../../shared/components/NavBar/NavBar";
 import SearchInput from "../../shared/components/SearchInput/SearchInput";
+import DepositInput from "../../shared/components/DepositInput/DepositInput";
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
 
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    paper: {
+        padding: theme.spacing(2),
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+    },
+}));
 export function Home() {
 
 
     return (
-        <div>
-           <h2>Encuentra tu guardian</h2>
-            <SearchInput/>
+        <Grid container spacing={3}>
+            <Grid item xs={12}>
+                <h2 className="Search">Encuentra tu guardian</h2>
+            </Grid>
+            <Grid item xs={12}>
+                <SearchInput/>
+            </Grid>
+            <Grid item xs={6}>
+                <DepositInput/>
+            </Grid>
+            <Grid item xs={6}>
+                <DepositInput/>
+            </Grid>
+            <Grid item xs={6}>
+                <DepositInput/>
+            </Grid>
+            <Grid item xs={6}>
+                <DepositInput/>
+            </Grid>
             <BottomNav/>
-        </div>
+        </Grid>
+
 
     )
 
--- src/shared/components/DepositInput/DepositInput.jsx
+++ src/shared/components/DepositInput/DepositInput.jsx
@@ -1,21 +1,20 @@
-import "./SearchInput.scss"
+import "./DepositInput.scss"
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import InputBase from '@material-ui/core/InputBase';
 import IconButton from '@material-ui/core/IconButton';
-import SearchIcon from '@material-ui/icons/Search';
+import EventNoteIcon from '@material-ui/icons/EventNote';
 
 const useStyles = makeStyles((theme) => ({
     root: {
         padding: '4px 4px',
         display: 'flex',
         alignItems: 'center',
-        width: '85vh',
+        width: '42vh',
     },
     input: {
         marginLeft: theme.spacing(1),
-        margin: theme.spacing(2),
         flex: 1,
     },
     iconButton: {
@@ -30,12 +29,12 @@ export default function CustomizedInputBase() {
 
     return (
         <Paper component="form" className={classes.root}>
-            <IconButton type="submit" className={classes.iconButton} aria-label="search">
-                <SearchIcon />
+            <IconButton type="date" className={classes.iconButton} aria-label="date">
+                <EventNoteIcon/>
             </IconButton>
             <InputBase
                 className={classes.input}
-                placeholder="¿Dónde te encuentras? Jerez de la Frontera"
+                placeholder="Depósito"
                 inputProps={{ 'aria-label': 'search google maps' }}
             />
         </Paper>
